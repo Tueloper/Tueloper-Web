ff5f85dee0317c4223534a17d3c0e6b549ce3120
Tueloper <ozurumbatochukwu@yahoo.com>
2019-11-01 02:53:38 +0100

    ft/template config setup complete

--- test/component.spec.js
+++ test/component.spec.js
@@ -1,7 +1,7 @@
 import { mount } from '@vue/test-utils'
 import Logo from '@/components/Logo.vue'
 import Header from '@/components/template/header.vue'
-import Loader from '@/components/loder.vue'
+import Loader from '@/components/loader.vue'
 
 describe('Logo', () => {
   test('is a Vue instance', () => {
