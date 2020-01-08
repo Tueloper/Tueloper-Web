61900461beb10710fca0702705dcd78bdf254b55
Tueloper <ozurumbatochukwu@yahoo.com>
2020-01-08 22:35:05 +0100

    contact page editted

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,22 +21,22 @@ export default {
 			// 	rel: 'stylesheet',
 			// 	href: 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'
 			// }
-		]
+		],
 
-		// script: [
-		// 	{
-		// 		src: 'https://code.jquery.com/jquery-3.3.1.slim.min.js',
-		// 		type: 'text/javascript'
-		// 	},
-		// 	{
-		// 		src: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js',
-		// 		type: 'text/javascript'
-		// 	},
-		// 	{
-		// 		src: 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js',
-		// 		type: 'text/javascript'
-		// 	}
-		// ]
+		script: [
+			{
+				src: 'https://code.jquery.com/jquery-3.3.1.slim.min.js',
+				type: 'text/javascript'
+			},
+			{
+				src: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js',
+				type: 'text/javascript'
+			},
+			{
+				src: 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js',
+				type: 'text/javascript'
+			}
+		]
 	},
 	/*
   ** Customize the progress-bar color
