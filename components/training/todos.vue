24f79430a0c7f9ffd6fd6417f15ea43c505c66fd
Tueloper <ozurumbatochukwu@yahoo.com>
2020-01-24 15:16:51 +0100

    vue basics

---
+++ components/training/todos.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="col-lg-3 col-md-3">
+      <div class="fact-box">
+        <div class="fact-content">
+          <h1>{{item.heading}}</h1>
+          <h2>{{item.id}}</h2>
+          <p>{{item.text}}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todos',
+  props: ['item']
+}
+</script>
+
+<style scoped>
+/* .fact-box {
+  width: auto;
+} */
+</style>
---
+++ pages/train/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <div class="text-center bg-dark welcome p-3">
+      <h3>{{welcome}}</h3>
+    </div>
+    <button class="btn btn-outline-dark" @click="showCaseToggle">Click Me</button>
+    <div v-if="showCase" v-bind:title="message">Hover On me</div>
+
+    <!-- for loop directive -->
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">{{todo.text}}</li>
+    </ul>
+
+    <!-- two way binding -->
+    <h4>{{display}}</h4>
+    <input type="text" v-model="display" class="form-control" />
+
+    <div class="row flex justify-content-center">
+      <todoItem v-for="list in groceryList" :item="list" :key="list.id" />
+    </div>
+  </div>
+</template>
+<script>
+import todoItem from './../../components/training/todos'
+
+export default {
+  layout: 'blog',
+
+  components: {
+    todoItem
+  },
+
+  data() {
+    return {
+      welcome: 'Training',
+      message: `This was loaded at ${new Date().toLocaleString()}`,
+      showCase: false,
+      todos: [{ text: 'Learn Javascript' }, { text: 'Learn Vue' }],
+      display: this.display,
+      groceryList: [
+        { id: 0, text: 'Vegetables', heading: 'VegiHead' },
+        { id: 1, text: 'Cheese', heading: 'CheeseHead' },
+        {
+          id: 2,
+          text: 'Whatever else humans are supposed to eat',
+          heading: 'WhateverHead'
+        }
+      ]
+    }
+  },
+
+  methods: {
+    showCaseToggle() {
+      this.showCase = !this.showCase
+    }
+  }
+}
+</script>
+
+<style scoped>
+.welcome h3 {
+  color: white !important;
+}
+</style>
