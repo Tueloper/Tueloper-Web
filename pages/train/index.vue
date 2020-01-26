b4ef33ef034b49529c3e6abc2f0dec79fc0e9056
Tueloper <ozurumbatochukwu@yahoo.com>
2020-01-26 16:04:52 +0100

    training continues components

--- pages/train/index.vue
+++ pages/train/index.vue
@@ -22,17 +22,55 @@
     <!-- <div class="text-center">
       <quiz />
     </div>-->
+
+    <template v-if="Math.random() > 2 ">
+      <h1>Title</h1>
+      <p>Paragraph 1</p>
+      <p>Paragraph 2</p>
+    </template>
+    <div v-else>There are numbers less 2</div>
+
+    <ul v-for="set in sets" :key="set.id">
+      <li v-for="n in even(set)">{{ n }}</li>
+    </ul>
+
+    <todoLoop>
+      <li>
+        {{title}}
+        <button class="btn btn-danger" @click="$emit('remove')">Remove</button>
+      </li>
+    </todoLoop>
+
+    <div>
+      <button @click.prevent="greet">Greet</button>
+    </div>
+
+    <!-- only call `vm.submit()` when the `key` is `Enter` -->
+    <input v-model="enval" v-on:keyup.enter="submit(enval)" />
+
+    <!-- using of communication between child and parent component -->
+    <div class="row" style="width: 100%;" :style="{ fontSize: postFontSize + 'em'}">
+      <Blog
+        v-for="post of posts"
+        :key="post.id"
+        :post="post"
+        v-on:enlarge="postFontSize += $event"
+      />
+    </div>
   </div>
 </template>
 <script>
 import todoItem from './../../components/training/todos'
-import quiz from './../../components/training/quiz'
+import Blog from './../../components/training/blog'
+import todoLoop from './../../components/training/loop'
+
 export default {
   layout: 'blog',
 
   components: {
     todoItem,
-    quiz
+    Blog,
+    todoLoop
   },
 
   data() {
@@ -52,13 +90,61 @@ export default {
         }
       ],
       url: '/',
-      attributeName: 'href'
+      attributeName: 'href',
+      sets: [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
+      new_todo: '',
+      todors: [
+        {
+          id: 1,
+          title: 'Do the dishes'
+        },
+        {
+          id: 2,
+          title: 'Take out the trash'
+        },
+        {
+          id: 3,
+          title: 'Mow the lawn'
+        }
+      ],
+      nextTodoId: 4,
+      posts: [
+        {
+          id: 1,
+          title: 'My journey with Vue',
+          content: 'This is where i learnt some crazy things'
+        },
+        {
+          id: 2,
+          title: 'Blogging with Vue',
+          content: 'This is where i learnt some crazy things'
+        },
+        {
+          id: 3,
+          title: 'Why Vue is so fun',
+          content: 'This is where i learnt some crazy things'
+        }
+      ],
+      postFontSize: 1
     }
   },
 
   methods: {
     showCaseToggle() {
       this.showCase = !this.showCase
+    },
+    even(numbers) {
+      return numbers.filter(number => {
+        return number % 2 === 0
+      })
+    },
+
+    greet(e) {
+      alert('Hello Tochi')
+      alert(`I am ${e.target.tagName}`)
+    },
+    submit(enval) {
+      alert(enval)
     }
   }
 }
